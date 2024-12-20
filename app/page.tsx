eee4106e985ed842ada2c36868d858ea91cdd95a
mubtesh <mubarekteshome@gmail.com>
2024-12-20 14:50:53 +0300

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   console.log("I am client or server?")
   return (
    <div>
-    <h1 className="text-3xl items-center">Hello World!</h1>
+    <h1 className="text-3xl items-center">Hello World! there</h1>
     <Hello />
    </div>
   );
